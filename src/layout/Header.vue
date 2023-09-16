85f8ed75b28555948f4c3c7ef62361bc344fb91f
suhye0n <contact.suhyeon@gmail.com>
2023-09-17 00:18:04 +0900

    deploy

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -49,3 +49,34 @@ const App = {
 
 export default App;
 </script>
+
+<style>
+* {
+    scroll-behavior: smooth;
+}
+
+a {
+    text-decoration-line: none;
+    color: var(--font-color);
+}
+
+.header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    grid-area: header;
+    width: 100%;
+    padding: 11px 40px;
+    background: var(--header-background-color);
+    box-shadow: 0px 2px 4px 0px var(--shadow-color);
+}
+
+.header-btn {
+    display: inline-flex;
+    margin-left: auto;
+    gap: 20px;
+    font-size: 14px;
+}
+</style>
